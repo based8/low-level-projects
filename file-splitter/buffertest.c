ee8dd6a1d7dfef102c5a640c6c573db4b12d3556
artixman <sudo.maneki@proton.me>
2024-10-15 16:07:27 -0400

    new repo new program

---
+++ file-splitter/buffertest.c
@@ -0,0 +1,38 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdint.h>
+
+int main() {
+	int ifd;
+	int ofd[3];
+	int length = sizeof(ofd) / sizeof(int);
+	char strbuff[1024]; 
+	char namebuff[64] = {'o','u','t','p'};
+
+	if ((ifd = open("data/example.txt", O_RDONLY)) == -1){
+		perror("could not open input file");
+		return 0;
+	}
+	if ((read(ifd, strbuff, sizeof(strbuff))) == -1){
+		perror("could not read input file");
+		return 0;
+	}
+	printf("\n\n %s \n\n", strbuff);
+	for (int i; i < length; i++)
+	{
+		namebuff[4] = i + '0';
+		if ((ofd[i] = open(namebuff, O_CREAT | O_RDWR )) == -1){
+			perror("could not create file");
+			return 0;
+		}
+		printf("Opened fd: %d, path: ",ofd[i]);
+		printf("%s \n", namebuff);
+	}
+	for (int i; i < length; i++)
+	{
+		close(ofd[i]);
+		printf("Closed fd: %i \n", ofd[i]);
+	}
+}
+
